f56e2550910058ec2c5877c022cf44173f14a355
Bahram Hajian <bahajian@gmail.com>
2025-02-08 15:34:29 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+let userGoal = 'Print Out the imported value';
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <link rel="stylesheet" href="styles.css">
+      </head>
+      <body>
+        <section>
+          <h2 style="color: yellow;">Hello Test App in YELLOW</h2>
+          <h3>${userGoal}</h3>
+        </section>
+        <form action="/store-goal" method="POST">
+          <div class="form-control">
+            <label>Imported Value</label>
+            <input type="text" name="goal">
+          </div>
+          <button>Set the Value</button>
+        </form>
+      </body>
+    </html>
+  `);
+});
+
+app.post('/store-goal', (req, res) => {
+  const enteredGoal = req.body.goal;
+  console.log(enteredGoal);
+  userGoal = enteredGoal;
+  res.redirect('/');
+});
+
+app.listen(80);
